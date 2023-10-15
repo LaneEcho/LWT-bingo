3fa910c6158550b8c0152eb81bc66765f3db88a7
Lane Hamilton <aleyna.hamilton@gmail.com>
2023-10-15 14:17:33 -0700

    random number function

--- src/client/components/board.tsx
+++ src/client/components/board.tsx
@@ -5,7 +5,33 @@ import Button from '@mui/material/Button';
 
 function Board() {
   // is there any state?
-  // render the rows
+
+  // function for pulling in phrases
+  // input: none
+  // output: array of random numbers with no repeats
+  // create a set so all numbers are unique
+  // loop to fill set with 24 numbers
+  // add to set
+  // return as an array
+
+  function pickUniqueNumbers(): number[] {
+    let uniqueNumbers: Set<number> = new Set();
+
+    while (uniqueNumbers.size < 24) {
+      let randomNumber: number = Math.floor(Math.random() * 48) + 1;
+      uniqueNumbers.add(randomNumber);
+    }
+
+    return Array.from(uniqueNumbers);
+  }
+
+  // iterate to create rows- an array of JSX elements
+  const rows: JSX.Element[] = [];
+  const phrasesIndex: number[] = pickUniqueNumbers();
+  console.log(phrasesIndex);
+
+  // function to reset board to different phrases
+
   return (
     <div className="board">
       <Button

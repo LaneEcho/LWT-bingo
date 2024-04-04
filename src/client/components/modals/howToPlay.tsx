f2e42d6d18602e2be3fb51cea87ec0b78a315a1a
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-04-03 21:49:59 -0700

    how to play

--- src/client/components/modals/howToPlay.tsx
+++ src/client/components/modals/howToPlay.tsx
@@ -50,28 +50,50 @@ const HowToPlay: React.FC<HowToPlayProps> = ({ close }) => {
           component="h2"
           gutterBottom
         >
-          How To Play Bingo
+          How To Play #LWT Bingo
         </Typography>
         <Typography variant="body1" gutterBottom>
-          body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-          blanditiis tenetur unde suscipit, quam beatae rerum inventore
-          consectetur, neque doloribus, cupiditate numquam dignissimos laborum
-          fugiat deleniti? Eum quasi quidem quibusdam.
+          Similar to classic Bingo, the goal is to mark squares on your board to
+          achieve a winning pattern. However, we reimagined to game to have a
+          #LWT twist!
         </Typography>
         <Typography variant="body1" gutterBottom>
-          body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-          blanditiis tenetur unde suscipit, quam beatae rerum inventore
-          consectetur, neque doloribus, cupiditate numquam dignissimos laborum
-          fugiat deleniti? Eum quasi quidem quibusdam.
+          You can play one board at a time. Each board has 25 playable squares
+          in a 5x5 square. The middle square is a free space!
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          Each square has a phrase, visual, or experience that might occur
+          during the #LWTSummit. If you see or hear that thing happen, click on
+          that square on your board to mark it!
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          Once you have enough squares marked to match a winning pattern, you
+          can click the "submit board" button to get a score, or keep playing to
+          try to match more complicated patterns for more points!
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          Points: Single line, any direction = 25 points # = 80 points "L" = 45
+          points "W" = 85 points "T" = 45 points "Blackout" (all the squares!) =
+          125 points!
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          When you submit your board, you have the option to submit your email
+          (so we can keep your scores tracked together) and the display name you
+          would like to use for the leaderboard.
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          After you submit, reset your board and play again!
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          You can reset your board at any time if you want a fresh start, but
+          any marked squares you have will also be reset.
         </Typography>
         <Button
           variant="contained"
           size="small"
           className="resetButton"
           onClick={handleClick}
-          sx={{
-            width: '8rem',
-          }}
+          sx={{}}
         >
           Let's Play
         </Button>

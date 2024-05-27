2cdeec018908be635913c9b5188faa2aa73ea230
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-05-26 20:58:02 -0700

    FocusTrap modals

--- src/client/components/modals/terms.tsx
+++ src/client/components/modals/terms.tsx
@@ -3,7 +3,7 @@ import { Box, Typography, Button, Stack } from '@mui/material';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
-import ListSubheader from '@mui/material/ListSubheader';
+import { FocusTrap } from '@mui/base/FocusTrap';
 import { ConditionsList } from '../../../lib/termsconditions';
 
 type TermsProps = {
@@ -43,50 +43,52 @@ const TermsAndConditions = React.forwardRef(function (
   }
 
   return (
-    <Box
-      sx={{
-        position: 'fixed',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: '40vw',
-        height: '60%',
-        bgcolor: 'background.paper',
-        padding: '1.5rem',
-        borderRadius: '8px',
-        textAlign: 'center',
-        overflow: 'scroll',
-      }}
-    >
-      <Stack
-        direction="column"
-        justifyContent="center"
-        alignItems="center"
-        spacing={2}
+    <FocusTrap open>
+      <Box
+        sx={{
+          position: 'fixed',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          width: '40vw',
+          height: '60%',
+          bgcolor: 'background.paper',
+          padding: '1.5rem',
+          borderRadius: '8px',
+          textAlign: 'center',
+          overflow: 'scroll',
+        }}
       >
-        <Typography id="modal-modal-title" variant="h4" component="h2">
-          Terms of Use
-        </Typography>
-        <List
-          sx={{
-            overflow: 'scroll',
-          }}
+        <Stack
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+          spacing={2}
         >
-          {listItems}
-        </List>
-        <Button
-          variant="contained"
-          size="small"
-          className="resetButton"
-          onClick={handleClick}
-          sx={{
-            width: '8rem',
-          }}
-        >
-          Got it!
-        </Button>
-      </Stack>
-    </Box>
+          <Typography id="modal-modal-title" variant="h4" component="h2">
+            Terms of Use
+          </Typography>
+          <List
+            sx={{
+              overflow: 'scroll',
+            }}
+          >
+            {listItems}
+          </List>
+          <Button
+            variant="contained"
+            size="small"
+            className="resetButton"
+            onClick={handleClick}
+            sx={{
+              width: '8rem',
+            }}
+          >
+            Got it!
+          </Button>
+        </Stack>
+      </Box>
+    </FocusTrap>
   );
 });
 

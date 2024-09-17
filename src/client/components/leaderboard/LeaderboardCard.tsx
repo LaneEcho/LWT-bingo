c76b263e57b76c82af3a5256308b2e208c812dad
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-09-16 23:46:15 -0400

    leaderboard style tweaks

--- src/client/components/leaderboard/LeaderboardCard.tsx
+++ src/client/components/leaderboard/LeaderboardCard.tsx
@@ -46,10 +46,7 @@ const LeaderboardCard: FunctionComponent<LeaderboardCardProps> = ({
         <Grid item flex={2}>
           {rank ?? index + 1}
         </Grid>
-        <Grid item 
-          flex={6} 
-          sx={{textOverflow:'ellipsis'}}
-          >
+        <Grid item flex={6} sx={{ textOverflow: 'ellipsis' }}>
           {score?.username}
         </Grid>
         <Grid
@@ -66,7 +63,10 @@ const LeaderboardCard: FunctionComponent<LeaderboardCardProps> = ({
   );
 
   function getRowColor(index: number) {
-    const colors = [theme.palette.primary.main, theme.palette.secondary.main];
+    // const colors = [theme.palette.primary.main, theme.palette.secondary.main];
+    // quick fix to stop having the colors swap up when changing modes
+    // TODO: do it better
+    const colors = ['#05fff4', '#E11774'];
 
     // Cycle through colors for other positions.
     // Overkill for just two colors, but leaving in case we decide to add a third or more in the future.

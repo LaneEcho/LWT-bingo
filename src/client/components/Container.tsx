160002a055b224d24ad5fbc1407a187435533d24
Lane Hamilton <aleyna.hamilton@gmail.com>
2025-01-24 21:38:10 -0800

    Container component

---
+++ src/client/components/Container.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Board from '../components/game_elements/board';
+import { useTheme } from '@mui/material';
+import { Box } from '@mui/material/';
+import Header from './Header';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import Leaderboard from '../components/leaderboard/leaderboard';
+
+export default function Container() {
+  const theme = useTheme();
+
+  const isMobile: boolean = useMediaQuery(theme.breakpoints.down('sm'));
+
+  return (
+    <>
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        sx={{
+          background: theme.palette.primary.contrastText,
+        }}
+      >
+        <Header />
+        <Box
+          display="flex"
+          gap={2}
+          justifyContent={'center'}
+          alignItems={isMobile ? 'center' : 'flex-start'}
+          flexDirection={isMobile ? 'column' : 'row'}
+        >
+          <Board />
+          <Leaderboard />
+        </Box>
+      </Box>
+    </>
+  );
+}

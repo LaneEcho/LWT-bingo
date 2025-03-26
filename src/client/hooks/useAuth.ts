3e18bc87cee8d6d3b52a16191d9584a56f514f6e
Lane Hamilton <112030625+LaneEcho@users.noreply.github.com>
2025-03-25 21:30:19 -0700

    Revert "Case Errors (Final Final?) "

--- src/client/hooks/useAuth.ts
+++ src/client/hooks/useAuth.ts
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { AuthContext } from '../context/authContext';
+import { AuthContext } from '../context/AuthContext';
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
